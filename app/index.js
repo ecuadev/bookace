de0dc68a281414289860c52bcd85d10ab0c398f9
David Bergmann <davidbepa@gmail.com>
2019-03-16 20:48:59 -0500

    Add and set up React Native Navigation

--- app/index.js
+++ app/index.js
@@ -2,8 +2,17 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { Navigation } from "react-native-navigation";
+import App from './src/App';
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent(`bookace.WelcomeScreen`, () => App);
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: "bookace.WelcomeScreen"
+      }
+    }
+  });
+});
