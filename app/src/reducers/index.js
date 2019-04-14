9bdd38f1d7da2b750db11daed028c428702ef5be
Esteban <esteban_fs@hotmail.com>
2019-04-14 14:51:12 -0500

    fixed reducer

--- app/src/reducers/index.js
+++ app/src/reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 
 import global from './global';
 import network from './network';
+import books from './books';
 import user from './user';
 import activeTab from './activeTab';
 
 export default combineReducers({
 	global,
 	network,
+	books,
 	user,
 	activeTab,
 });
